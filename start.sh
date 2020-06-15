6c4fd898ea7bc50cb80018748f9dd65cb86c8dc6
weigel <rweigel@gmu.edu>
2020-06-15 15:41:01 -0400

    icons

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 source ~/.nvm/nvm.sh && nvm use 8
 
 if [ "$1" = "devel" ]; then
-    nodemon verify.js --port 9997 2>&1 &
+    nodemon verify.js --port 9997 --plotserver http://localhost:5000/
 else
     nohup forever verify.js --port 9997 2>&1 &
     tail -f nohup.out

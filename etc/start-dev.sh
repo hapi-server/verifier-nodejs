bd4f5b4ec8b2d6abdc3a8819777848fa9fb2733e
Ubuntu <rweigel@gmu.edu>
2023-03-29 17:13:07 +0000

    service config

--- etc/start-dev.sh
+++ etc/start-dev.sh
@@ -4,7 +4,6 @@ source ~/.nvm/nvm.sh && nvm use 16
 if [ "$1" = "devel" ]; then
     nodemon verify.js --port 9996 --plotserver http://localhost:5000/
 else
-    cpulimit -l 5 -- nice -20 node --max-old-space-size=96 verify.js --port 9996 
-    #nohup forever verify.js --port 9996 2>&1 &
-    #tail -f nohup.out
+    #cpulimit -l 5 -- nice -20 node --max-old-space-size=96 verify.js --port 9996 
+    node --max-old-space-size=96 verify.js --port 9996
 fi

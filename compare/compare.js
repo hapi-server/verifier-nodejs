8030370ee4b764bc55db221ab9943a3589429dcb
Bob Weigel <rweigel@gmu.edu>
2023-04-08 09:40:41 -0400

    /info compare

--- compare/compare.js
+++ compare/compare.js
@@ -14,7 +14,8 @@ const argv =
             "parameters": "scalar",
             "start": "1970-01-01Z",
             "stop": "1970-01-01T00:00:11.000Z",
-            "simulate": true,
+            "simulate": false,
+            "showpasses": false,
             "timefraction": "0.5"
           })
           .option('simulate',{'type': 'boolean'})
@@ -22,31 +23,54 @@ const argv =
 
 let report = require('./report.js').report;
 
+let args_a = `?id=${argv['dataset']}&parameters=${argv['parameters']}`;
+let args_b = `&time.min=${argv['start']}&time.max=${argv['stop']}`;
+
 let url1 = argv['server1'].replace(/\/$/,"") + "/" + argv['endpoint'];
 let url2 = argv['server2'].replace(/\/$/,"") + "/" + argv['endpoint'];
-url1 = url1 + "?id=" + argv['dataset'] + "&parameters=" + argv['parameters'] + "&time.min=" + argv['start'] + "&time.max=" + argv['stop'];
-url2 = url2 + "?id=" + argv['dataset'] + "&parameters=" + argv['parameters'] + "&time.min=" + argv['start'] + "&time.max=" + argv['stop'];
+url1 = url1 + args_a + args_b;
+url2 = url2 + args_a + args_b;
 
 let url1i = argv['server1'].replace(/\/$/,"") + "/info";
-url1i = url1i + "?id=" + argv['dataset'] + "&parameters=" + argv['parameters'];
+let url2i = argv['server1'].replace(/\/$/,"") + "/info";
+url1i = url1i + args_a;
+url2i = url2i + args_a;
 
-//Using Callbacks
-let series = 
-              [
-                function(cb) {get(url1, cb)},
-                function(cb) {get(url2, cb)}
-              ];
+let seriesi = [(cb) => {get(url1i, "1 ", cb)}, (cb) => {get(url2i, "2 ", cb)}];
+let seriesd = [(cb) => {get(url1, "1 ", cb)}, (cb) => {get(url2, "2 ", cb)}];
 
-get(url1i, (err, data) => {
-  async.series(series, 
-    (err, results) => {
-      report(argv, JSON.parse(data.body), results);
-    });  
-})
 
-function get(url, cb) {
+async.series(seriesi, 
 
-  console.log(clc.blue('Getting ' + url + '\n'));
+  (err, infos) => {
+    if (err) {
+      console.log("Error:")
+      console.error(err);
+      console.log("Exiting with code 1")
+      process.exit(1);
+    }
+
+    infos[0] = JSON.parse(infos[0]['body']);
+    infos[1] = JSON.parse(infos[1]['body']);
+
+    async.series(seriesd, 
+      (err, data) => {
+        if (err) {
+          console.log("Error:")
+          console.error(err);
+          console.log("Exiting with code 1")
+          process.exit(1);
+        }
+        report(argv, infos, data);
+    });
+
+});
+
+
+function get(url, msgPrefix, cb) {
+
+  console.log(msgPrefix + clc.blue('Getting ' + url));
+
   let opts = {"url": url, "time": true};
   request(opts,
     function (err,res,body) {
@@ -54,25 +78,29 @@ function get(url, cb) {
                     {
                       "url": url,
                       "headers": res.headers,
-                      "timing": extractTiming(res),
-                      "body": body
+                      "body": body,
+                      "timing": extractTiming(res)
                     }
         cb(err, data);
     });
 }
 
 function extractTiming(res, precision) {
-  // Remove nonsense extra precision on timings.
+
+  // Remove extra precision on timings.
   if (precision === undefined) {
-    precision = 1;
+    precision = 2;
   }
-  var timings = res.timings;
-  for (var key in timings) {
+
+  let timings = res.timings;
+  for (let key in timings) {
     timings[key] = timings[key].toFixed(precision);
   }
-  var timingPhases = res.timingPhases;
-  for (var key in timingPhases) {
+
+  let timingPhases = res.timingPhases;
+  for (let key in timingPhases) {
     timingPhases[key] = timingPhases[key].toFixed(precision);
   }
-  return {...timings, ...timingPhases}
+
+  return {...timings, ...timingPhases};
 }
--- compare/report.js
+++ compare/report.js
@@ -1,118 +1,286 @@
 const clc  = require('chalk');
 
-function report(argv, info, data) {
+function report(argv, infos, data) {
 
-  const SIMULATE = argv['simulate'];
-  const TIMEFRACTION = parseFloat(argv['timefraction']);
+  log.argv = argv;
 
-  console.log(clc.blue(`Timings (|diff|/ave values >= ${TIMEFRACTION})`));
+  let errs = 0;
+  //errs += timings(data[0]['timing'], data[1]['timing'], argv);
+  //errs += headers(data[0]['headers'], data[1]['headers'], argv)
 
-  let timingDiff = false;
-  for (let key of Object.keys(data[0]['timing'])) {
-    let val1 = parseFloat(data[0]['timing'][key]);
-    let val2 = parseFloat(data[1]['timing'][key]);
-    let timingDiff = true;
-    let ave = (val1 + val2);
-    if (Math.abs(val1 - val2)/ave >= TIMEFRACTION) {
-      console.log(` 1 ${key} = ${val1}`);
-      console.log(` 2 ${key} = ${val2}`);
-    }
-  }
-  if (timingDiff === false) {
-    console.log(` No |diff|/ave values >= ${TIMEFRACTION}`);
-  }
+  // Get matching info parameters until difference
+  infoc = info(infos, argv);
+  process.exit(0);
+  let ct1 = data[0]['headers']['content-type'];
+  let ct2 = data[1]['headers']['content-type'];
 
-  console.log(clc.blue("HTTP Header"));
-  let headerDiff = false;
-
-  let h1 = data[0]['headers'];
-  let h2 = data[1]['headers'];
-  delete h1['date'];
-  delete h2['date'];
-
-  if (SIMULATE) delete h1['transfer-encoding']
-
-  let seen = [];
-  for (let key of Object.keys(h1)) {
-    if (h1[key] !== h2[key]) {
-      headerDiff = true;
-      console.log(` 1 ${key} = ${h1[key]}`);
-      console.log(` 2 ${key} = ${h2[key]}`);
-    }
-    seen.push(key);
-  }
-  for (let key of Object.keys(h2)) {
-    if (seen.includes(key)) continue;
-    headerDiff = true;
-    console.log(` 1 ${key} = ${h1[key]}`);
-    console.log(` 2 ${key} = ${h2[key]}`);
-  }
-  if (headerDiff === false) {
-    console.log(" No differences");
-  }
-
-  if (h1['content-type'].includes('csv') && h2['content-type'].includes('csv')) {
-
-    console.log(clc.blue("CSV Data"))
-
-    b1 = data[0]['body'].split("\n");
-    b2 = data[1]['body'].split("\n");
-
-    bodyNLinesDiff = false;
-    if (b1.length !== b2.length) {
-      console.log(` 1 ${b1.length} lines`);
-      console.log(` 2 ${b2.length} lines`);
-      bodyNLinesDiff = true;
-    }
-
-    let bodyExactDiff = false;
-    for (let i = 0; i < b1.length; i++) {
-      if (b1[i] !== b2[i]) {
-        bodyExactDiff = true;
-        console.log(` 1 line ${i}: ${b1[i]}`);
-        console.log(` 2 line ${i}: ${b2[i]}`);
-      }
-    }
-
-    let bodyValDiff = false;
-    let bodyNColsDiff = false;
-    for (let i = 0; i < b1.length; i++) {
-      let cols1 = b1[i].split(",");
-      let cols2 = b2[i].split(",");
-      if (cols1.length !== cols2.length) {
-          console.log(` 1 line ${i} ${cols1.length} columns`);
-          console.log(` 2 line ${i} ${cols2.length} columns`);
-          bodyNColsDiff = true;
-          break;        
-      }
-      for (let j = 0; j < cols1.length; j++) {
-        if (SIMULATE) cols1[j] = cols1[j] + "9";
-
-        cols1p = cols1[j].trim();
-        cols2p = cols2[j].trim();
-
-        parsed = false;
-        if (j > 0) {
-          // TODO: Determine if float from /info response.
-          parsed = true;
-          cols1p = parseFloat(cols1[j]);
-          cols2p = parseFloat(cols2[j]);
-        }
-        if (cols1p !== cols2p) {
-          bodyValDiff = true;
-          if (parsed === true) {
-            console.log(` 1 line ${i} col ${j}: raw: ${cols1[j]}`);
-            console.log(`              parsed: ${cols1p}`);
-            console.log(` 2 line ${i} col ${j}: raw: ${cols2[j]}`);
-            console.log(`              parsed: ${cols2p}`);
-          }
-        }
-      }
-    }
-    if (bodyNLinesDiff === false && bodyExactDiff === false
-        && bodyValDiff === false && bodyNColsDiff === false) {
-      console.log(" No differences");
-    }
+  if (ct1.includes('csv') && ct2.includes('csv')) {
+    errs += csv(data[0]['body'], data[1]['body'], infoc, argv);
+  } else {
+    log("content-types are not both csv",true);
+    errs += 1;
   }
 }
 exports.report = report;
+
+function log(msg, msgType) {
+  if (msgType === true)
+    console.log(clc.red(' ✗ ') + msg);
+  if (msgType === false && log.argv['showpasses'] === true)
+    console.log(clc.green(' ✓ ') + msg);
+  if (msgType === 'title')
+    console.log("\n" + clc.blue(msg));
+  if (msgType === 'pass-summary')
+    console.log(clc.green(' ✓ ' + msg));
+  if (msgType === 'fail-summary')
+    console.log(clc.red(' ✗ ' + msg));
+}
+
+function timings(timing1, timing2, argv) {
+
+  const TIMEFRACTION = parseFloat(argv['timefraction']);
+
+  log(`Timings (|diff|/ave values >= ${TIMEFRACTION})`,'title');
+
+  let timingDiffs = 0;
+  for (let key of Object.keys(timing1)) {
+  
+    timingDiff = false;
+
+    let val1 = parseFloat(timing1[key]);
+    let val2 = parseFloat(timing2[key]);
+    let ave = (val1 + val2);
+
+    if (Math.abs(val1 - val2)/ave >= TIMEFRACTION) {
+      timingDiff = true;
+      timingDiffs = timingDiffs + 1;
+    }
+
+    log(`1 ${key} = ${val1}`,timingDiff);
+    log(`2 ${key} = ${val2}`,timingDiff);
+  }
+
+  let msg = `${timingDiffs} |diff|/ave values >= ${TIMEFRACTION}`;
+  console.log('-'.repeat(4));
+  if (timingDiffs === 0) {
+    log(msg,'pass-summary');
+    return 0;
+  } else {
+    log(msg,'fail-summary');
+    return 1;
+  }
+}
+
+function headers(h1, h2, argv) {
+
+  log("HTTP Header",'title');
+
+  delete h1['date'];
+  delete h2['date'];
+
+  if (argv['simulate']) delete h1['transfer-encoding']
+
+  let headerDiffs = 0;
+  let seen = [];
+  for (let key of Object.keys(h1)) {
+    let headerDiff = false;
+    if (h1[key] !== h2[key]) {
+      headerDiff = true;
+      headerDiffs = headerDiffs + 1;
+    }
+    log(`1 ${key} = ${h1[key]}`,headerDiff);
+    log(`2 ${key} = ${h2[key]}`,headerDiff);
+    seen.push(key);
+  }
+
+  for (let key of Object.keys(h2)) {
+    if (seen.includes(key)) continue;
+    headerDiff = true;
+    headerDiffs = headerDiffs + 1;
+    log(`1 ${key} = ${h1[key]}`,headerDiff);
+    log(`2 ${key} = ${h2[key]}`,headerDiff);
+  }
+
+  let msg = `${headerDiffs} differences`;
+  console.log('-'.repeat(4));
+  if (headerDiffs === 0) {
+    log(msg,'pass-summary');
+    return 0;
+  } else {
+    log(msg,'fail-summary');
+    return 1;
+  }
+}
+
+function info(infos, argv) {
+
+  function equals(a,b,name) {
+
+    if (a === null && b === null) {
+      return true;
+    }
+
+    if ('string' == typeof a == typeof b) {
+      if (a == b) return true;
+      return false;
+    }
+
+    if (Array.isArray(a) && Array.isArray(b)) {
+      let diffs = 0;
+      for (let i = 0; i < Math.min(a.length,b.length); i++) {
+        let diff = false;
+        if (!equals(a[i], b[i], `${name}[${i}]`)) {
+          let diff = true;
+          diffs += 1;
+        }
+        //log(`1 ${name}[${i}] = ${a[i]}`,diff);
+        //log(`2 ${name}[${i}] = ${b[i]}`,diff);
+      }
+      return diffs.length > 0;
+    }
+
+    if (typeof a === 'object' && typeof b === 'object') {
+      if (a === null || b === null) return 1;
+
+      let diffs = 0;
+      let seen = [];
+      for (let key of Object.keys(a)) {
+        let diff = false;
+        if (!equals(a[key], b[key], `${name}[${key}]`)) {
+          diff = true;
+          diffs = diffs + 1;
+        }
+        if (typeof a[key] !== 'object' && a !== null) {
+          log(`1 ${name}['${key}'] = ${a[key]}`,diff);
+          log(`2 ${name}['${key}'] = ${b[key]}`,diff);
+        }
+        seen.push(key);
+      }
+      for (let key of Object.keys(b)) {
+        if (seen.includes(key)) continue;
+        diff = true;
+        diffs = diffs + 1;
+        log(`1 ${key} = ${a[key]}`,diff);
+        log(`2 ${key} = ${b[key]}`,diff);
+      }
+      return diffs.length > 0;
+    }
+
+    return true;
+  }
+
+  log("/info",'title');
+
+  if (argv['simulate']) {
+    delete infos[0]['startDate'];
+    infos[0]['parameters'][0]['name'] += 'x';
+  }
+  delete infos[0]['status'];
+  delete infos[1]['status'];
+  return equals(infos[0],infos[1],'info');
+}
+
+function csv(b1, b2, infoc, argv) {
+
+  log("CSV Data",'title');
+
+  if (argv['simulate']) {
+    b1 = b1.trimEnd();
+    b2 = b2.trimEnd();
+    b1 = b1.split("\n");
+    b1 = b1.slice(0,b1.length-1);
+    b1[1] = b1[1] + "9";
+    b1 = b1.join("\n");
+    //console.log(b1);
+    //console.log(b2);
+  }
+
+  if (b1 === b2) {
+    log("No differences",'pass-summary');
+    return 0;
+  }
+
+  b1 = b1.split("\n");
+  b2 = b2.split("\n");
+
+  bodyNLinesDiff = false;
+  if (b1.length !== b2.length) {
+    bodyNLinesDiff = true;
+  }
+  log(`1 ${b1.length} lines`,bodyNLinesDiff);
+  log(`2 ${b2.length} lines`,bodyNLinesDiff);
+
+  let bodyLineDiffs = 0;
+  for (let i = 0; i < Math.min(b1.length,b2.length); i++) {
+    let bodyLineDiff = false;
+    if (b1[i] !== b2[i]) {
+      bodyLineDiff = true;
+      bodyLineDiffs = bodyLineDiffs + 1;
+    }
+    log(`1 line ${i}: ${b1[i]}`,bodyLineDiff);
+    log(`2 line ${i}: ${b2[i]}`,bodyLineDiff);
+  }
+
+  let bodyValDiffs = 0;
+  let bodyNColsDiffs = 0;
+  for (let i = 0; i < b1.length; i++) {
+    let cols1 = b1[i].split(",");
+    let cols2 = b2[i].split(",");
+    let bodyNColsDiff = false;
+
+    if (cols1.length !== cols2.length) {
+      let bodyNColsDiff = true;
+      bodyNColsDiffs = bodyNColsDiffs + 1;
+    }
+    log(`1 line ${i} ${cols1.length} columns`,bodyNColsDiff);
+    log(`2 line ${i} ${cols2.length} columns`,bodyNColsDiff);
+
+    for (let j = 0; j < Math.min(cols1.length,cols2.length); j++) {
+
+      let bodyValDiff = false;
+
+      cols1p = cols1[j].trim();
+      cols2p = cols2[j].trim();
+
+      parsed = false;
+      if ('double' === infoc['parameters'][j]['type']) {
+        // TODO: Determine if float from /info response.
+        parsed = true;
+        cols1p = parseFloat(cols1[j]);
+        cols2p = parseFloat(cols2[j]);
+      }
+
+      if (cols1p !== cols2p) {
+        bodyValDiff = true;
+        bodyValDiffs = bodyValDiffs + 1;
+      }
+
+      if (parsed === true) {
+        log(`1 line ${i} col ${j}:    raw: ${cols1[j]}`,bodyValDiff);
+        log(`                parsed: ${cols1p}`,bodyValDiff);
+        log(`2 line ${i} col ${j}:    raw: ${cols2[j]}`,bodyValDiff);
+        log(`                parsed: ${cols2p}`,bodyValDiff);
+      } else {
+        log(`1 line ${i} col ${j}: ${cols1[j]}`,bodyValDiff);
+        log(`2 line ${i} col ${j}: ${cols2[j]}`,bodyValDiff);
+      }
+    }
+  }
+
+  console.log('-'.repeat(4));
+  if (bodyNLinesDiff === false && bodyLineDiffs === 0 &&
+      bodyNColsDiffs === 0 && bodyValDiffs === 0) {
+    log("No differences",'pass-summary');
+    return 0;
+  } else {
+    if (bodyNLinesDiff === true)
+      log("Number of lines differs",'fail-summary');
+    if (bodyLineDiffs > 0)
+      log(`${bodyLineDiffs} line(s) differs`,'fail-summary');
+    if (bodyNColsDiffs > 0)
+      log(`Number of columns differ`,'fail-summary');
+    if (bodyValDiffs > 0)
+      log(`${bodyValDiffs} column value(s) differs`,'fail-summary');
+    return 1;
+  }
+}
